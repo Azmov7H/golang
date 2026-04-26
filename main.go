cfb6c4cba38be882b2fd6910c4fc21bcf3328fb5
Azmov7H <alihak31328@gmail.com>
2026-04-27 00:52:09 +0300

    Add calculateArea function with arithmetic operations and boolean checks

--- main.go
+++ main.go
@@ -10,4 +10,25 @@ func main() {
 	var age int = 30
 	fmt.Println("Hello, " + userName + "!")
 	fmt.Println("Your age is: " + strconv.Itoa(age))
+	calculateArea()
+}
+
+func calculateArea()  {
+	a := 10
+	b := 5
+	area := a * b
+	plus := a + b
+	munus := a - b
+	division := a / b
+	// Convert integers to strings for printing
+	fmt.Println("The sum of a and b is: " + strconv.Itoa(plus))
+	fmt.Println("The area of the rectangle is: " + strconv.Itoa(area))
+	fmt.Println("The difference of a and b is: " + strconv.Itoa(munus))
+	fmt.Println("The division of a and b is: " + strconv.Itoa(division))
+	//Boolean
+	isGreater := a > b
+	notGreater := a < b
+	fmt.Println("Is a less than b? " + strconv.FormatBool(notGreater))
+	fmt.Println("Is a greater than b? " + strconv.FormatBool(isGreater))
+
 }
